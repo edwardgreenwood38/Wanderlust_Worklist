8c3e7588d7edfccdc4b7b6c9f3c3b0f6608354b4
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-26 21:04:25 -0400

    installed react-bootstrap and move title to top

---
+++ server/wanderlust_server/src/title.jsx
@@ -0,0 +1,14 @@
+//import React from 'react';
+import './App.css';
+
+
+function Title() {
+    return (
+        <div className="App">
+            <h1>WANDERLUST WORKLIST</h1>
+        </div>
+    );
+}
+
+
+export default Title;
